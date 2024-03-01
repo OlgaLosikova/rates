f3d36309704fea586c956cfe48101d034ffb5870
OlgaLosikova <techtigra@gmail.com>
2024-03-01 12:27:01 +0300

    click on the card

--- src/components/RateCard/RateCard.jsx
+++ src/components/RateCard/RateCard.jsx
@@ -1,19 +1,29 @@
 import styles from "./RateCard.module.css";
 import cn from "classnames";
+import { useState } from "react";
+
 const RateCard = (props) => {
-  const { header, price, speed, headerColor, contentColor, select } = props;
+  const { header, price, speed, comment, headerColor, contentColor} =
+    props;
+  const [cardSelect, setCardSelect] = useState(false);
+  const handleClick = () => {
+    setCardSelect(!cardSelect);
+  };
   const headerBg = cn(styles.h2, styles[headerColor]);
   const contentBg = cn(styles.container, styles[contentColor]);
-  const cardStyle = cn(styles.rateCard, { [styles.select]: select });
+  const cardStyle = cn(
+    styles.rateCard,
+    cardSelect && styles.select 
+  );
 
   return (
-    <div className={cardStyle}>
-      <h2 className={headerBg}>Безлимитный {header}</h2>
+    <div onClick={handleClick} className={cardStyle}>
+      <h2 className={headerBg}>{header}</h2>
       <p className={contentBg}>
         руб <span className={styles.price}>{price}</span> /мес
       </p>
-      <p>до {speed} Мбит/сек</p>
-      <p className={styles.footer}>Объем включенного трафика не ограничен</p>
+      <p>{speed}</p>
+      <p className={styles.footer}>{comment}</p>
     </div>
   );
 };
---
+++ src/data.js
@@ -0,0 +1,39 @@
+export const data = [
+  {
+    id: 1,
+    header: "Безлимитный 300",
+    price: 300,
+    speed: "до 10 Мбит/сек",
+    comment: "Объем включенного трафика не ограничен",
+    headerColor: "headerDefault",
+    contentColor: "contentDefault",
+  },
+  {
+    id: 2,
+    header: "Безлимитный 450",
+    price: 450,
+    speed: "до 50 Мбит/сек",
+    comment: "Объем включенного трафика не ограничен",
+    headerColor: "headerSuccess",
+    contentColor: "contentSuccess",
+  },
+  {
+    id: 3,
+    header: "Безлимитный 550",
+    price: 550,
+    speed: "до 100 Мбит/сек",
+    comment: "Объем включенного трафика не ограничен",
+    headerColor: "headerDanger",
+    contentColor: "contentDanger",
+
+  },
+  {
+    id: 4,
+    header: "Безлимитный 1000",
+    price: 1000,
+    speed: "до 200 Мбит/сек",
+    comment: "Объем включенного трафика не ограничен",
+    headerColor: "headerDark",
+    contentColor: "contentDark",
+  },
+];
